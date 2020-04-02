4eb8c2c65f71b90bcae3a98040555b21fad96dbc
stan96D <stan-deckers@live.nl>
2020-04-02 12:57:02 +0200

    profile added

---
+++ screens/coaching/coach.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView, ScrollView, TextInput, TouchableOpacity, Image, Linking, Alert } from 'react-native';
+import ProfilePicture from 'react-native-profile-picture';
+import { globalStyles, Colors } from '../../constants';
+import SendWhatsApp from '../../api/whatsapp';
+import {Avatar} from 'react-native-elements';
+
+const CoachCard = ({coach}) => {
+    return (
+        <View style={styles.coachContainer}>
+            <View style={styles.profileContainer}>
+                <Avatar source={{uri: coach?.photo}} size="large" rounded ></Avatar>
+            <Text style={styles.headerText}>{coach?.name}</Text>
+            <Text style={styles.subHeaderText}>{coach?.expertise}</Text>
+            </View>     
+            <View style={styles.textContainer}>
+            <View style={styles.quoteContainer}>
+                <Text style={styles.normalText}>"{coach?.quote}"</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={() => SendWhatsApp(coach?.phonenumber)}>
+                <Text style={styles.buttonText}>Stuur bericht</Text>
+                </TouchableOpacity>
+            </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    headerText: {
+      fontWeight: '700',
+      fontFamily: globalStyles.fontStyle.fontFamily,
+      marginTop: 10
+    },
+  
+    subHeaderText: {
+      fontWeight: '300',  
+    },
+  
+    buttonText: {
+      color: Colors.primary,
+      fontSize: 12,
+    },
+  
+    buttonContainer: {
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end',
+      width: '100%',
+      height: '20%',
+      paddingRight: 15,
+      paddingBottom: 15,
+    },
+  
+    quoteContainer: {
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      width: '100%',
+      height: '80%',
+      paddingTop: 20,
+      paddingRight: 20,
+    },
+  
+    button: {
+      height: 35,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'white',
+      borderColor: Colors.primary,
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10
+    },
+  
+    normalText: {
+      fontWeight: '100',
+      fontStyle: 'italic',
+      color: Colors.secondary,
+    },
+  
+  
+    coachContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'row',
+      borderBottomColor: 'lightgray',
+      borderBottomWidth: 0.25
+    },
+  
+    profileContainer: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'white',
+      flex: 3,
+      height: '100%'
+    },
+  
+    profile: {
+      width: '100%'
+    },
+  
+    textContainer: {
+      flex: 7,
+      backgroundColor: 'white',
+      height: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default CoachCard
+
--- screens/modals/articleModal.js
+++ screens/modals/articleModal.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { Modal, StyleSheet, View, TouchableOpacity, Text } from 'react-native';
-import { globalStyles, Colors } from '../../constants'; 
+import { Modal, StyleSheet, View, TouchableOpacity, Text, Image, ScrollView } from 'react-native';
+import { globalStyles, Colors, SCREEN_HEIGHT, SCREEN_WIDTH } from '../../constants'; 
+import { SocialIcon, Input, Button } from 'react-native-elements'
 
-const ArticleModal = ({ visible, onClose}) => {
+const ArticleModal = ({ article, visible, onClose}) => {
 
     return (
         <Modal animationType='slide' visible={visible}  >
-            <View style={{alignItems: 'center', justifyContent: 'center', height: '100%'}}>
-                <Text>Ik word een artikel</Text>
-                <TouchableOpacity onPress={onClose}>
-                    <Text style={{color: Colors.primary}}>Sluiten</Text>
-                </TouchableOpacity>
-            </View>
+            <ScrollView style={{ height: '100%', width: '100%'}}>
+                <View style={{ height: SCREEN_HEIGHT/2.5, width: '100%', opacity: '100%'}}>
+
+                    <Image source={{uri: article.image}} style={{height: '100%', width: '100%', borderRadius: 2,}}></Image>
+                    <TouchableOpacity onPress={onClose} style={{position: 'absolute', top: 40, right: 20, }}>
+                        <Image source={require('../../assets/cancelButton.png')} style={{tintColor: Colors.tertiary, height: 35, width: 35}}></Image>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ height: '100%', width: '100%'}}>
+                    <View style={{padding: 20}}>
+                        <Text style={{ color: Colors.secondary, fontWeight: '600', fontSize: 20}}>{article.title}</Text>
+                        <Text style={{ color: Colors.tertiary, fontWeight: '600', fontSize: 12, paddingBottom: 10}}>Auteur: {article.autor}</Text>
+                        <Text style={{ color: 'black', fontWeight: '600', fontSize: 14}}>{article.header}</Text>
+                    </View>
+                    <View style={{padding: 20, justifyContent: 'center', alignItems: 'center'}}>
+                        <Text>Content</Text>
+                    </View>
+                    <View style={{padding: 20, alignItems: 'center'} }>
+                        <Text style={{fontWeight: '100', fontStyle: 'italic', color: Colors.secondary}}>Wat vond je van het artikel?</Text>
+                        <SocialIcon
+                            Component={TouchableOpacity}
+                            style={{height: 40, width: 40}}
+                            type='facebook'
+                        />
+                    </View>
+                </View>
+            </ScrollView>
+
         </Modal>
     )
 }
